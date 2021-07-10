c32ed2b431899e375b4f660a53f53fa02699f349
dbswls627 <dbswls627@naver.com>
2021-07-10 17:20:41 +0900

    검색 구현

--- app/src/main/java/com/example/lolbox/MainActivity.kt
+++ app/src/main/java/com/example/lolbox/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.lolbox
 
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    val list = arrayListOf<Cham>()
+    val filteredList = arrayListOf<Cham>()
     val name= arrayListOf<String>("가렌", "갈리오","갱플랭크", "그라가스", "그레이브즈", "그웬",
         "나르", "나미", "나서스", "노틸러스", "녹턴", "누누와 윌럼프", "니달리", "니코",
         "다리우스", "다이애나", "드레이븐",
@@ -38,11 +40,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val list = arrayListOf<Cham>()
+
         for (index in img.indices ){
             list.add(Cham(img[index],name[index],false,false))
         }
         rv.layoutManager=LinearLayoutManager(this)
         rv.adapter=adapter(list)
+        search.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                val searchText: String = search.text.toString()
+                searchFilter(searchText)
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+            }
+        })
+        }
+    private fun searchFilter(searchText: String) {
+        filteredList.clear()
+        for (i in 0 until list.size) {
+            if (list[i].name.length >= searchText.length) {
+                if (list[i].name.substring(0, searchText.length).contains(searchText)) {
+                    filteredList.add(list[i])
+                }
+            }
+        }
+        rv.adapter=adapter(filteredList)
     }
 }
--- app/src/main/java/com/example/lolbox/adapter.kt
+++ app/src/main/java/com/example/lolbox/adapter.kt
@@ -2,21 +2,25 @@ package com.example.lolbox
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class Cham(val img: Int, val name: String, val box: Boolean, val save: Boolean)
+class Cham(val img: Int, val name: String, var box: Boolean, var save: Boolean)
 class CustuomViewHolder(v : View) : RecyclerView.ViewHolder(v){
     val img =v.img
     val name: TextView =v.name
 }
 
 class adapter(val list:ArrayList<Cham>):RecyclerView.Adapter<CustuomViewHolder>(){
+    var ArrayList: ArrayList<Cham>? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustuomViewHolder {
        val cellForRow=LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
         return CustuomViewHolder(cellForRow)
+    }
+    fun filterList(filteredList: ArrayList<Cham>?) {
+        ArrayList =filteredList
+        notifyDataSetChanged()
     }
 
     override fun getItemCount()=list.size
@@ -24,5 +28,7 @@ class adapter(val list:ArrayList<Cham>):RecyclerView.Adapter<CustuomViewHolder>(
     override fun onBindViewHolder(holder: CustuomViewHolder, position: Int) {
         holder.img.setImageResource(list[position].img)
         holder.name.setText(list[position].name)
+  
+        }
     }
 }
